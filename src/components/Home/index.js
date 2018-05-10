18af0bc25fcc20af9a3eaa6a0650d55aedf77a23
GunnlaugurCalvi <gunnlaugur15@ru.is>
2018-05-10 19:50:43 +0000

    filtered

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -24,6 +24,7 @@ class Home extends Component {
     async getBlocks(curr_block_no) {
         const block_ids = this.state.block_ids.slice();
         const block_hashes = this.state.block_hashes.slice();
+        const Tx = this.state.Tx; 
         let max_blocks = 10;
         
         if(curr_block_no > max_blocks){
@@ -34,34 +35,43 @@ class Home extends Component {
             let currBlockObj = await web3.eth.getBlock(curr_block_no);
             block_ids.push(currBlockObj.number);
             block_hashes.push(currBlockObj.hash);
+            Tx.push(currBlockObj.transactions);
         }
 
         this.setState({
             block_ids: block_ids,
-            block_hashes: block_hashes
+            block_hashes: block_hashes,
+            Tx: Tx
         });
 
     }
 
     render() {
         let tableRows = [];
+        let txCount = 0;
         _.each(this.state.block_ids, (value, index) => {
-            tableRows.push(
-                <tr key={this.state.block_hashes[index]}>
-                    <td className="tdCenter">{this.state.block_ids[index]}</td>
-                    <td><Link to={`/block/${this.state.block_hashes[index]}`}>{this.state.block_hashes[index]}</Link></td>
-                </tr>
-            )
+            if(this.state.Tx[index].length > 0){
+                tableRows.push(
+                    <tr key={this.state.block_hashes[index]}>
+                        <td className="tdCenter">{this.state.block_ids[index]}</td>
+                        <td className="tdCenter">{this.state.Tx[index]}</td>
+                        <td><Link to={`/block/${this.state.block_hashes[index]}`}>{this.state.block_hashes[index]}</Link></td>
+                    </tr>
+                )
+            }
+        txCount = tableRows.length;
+            
         });
         return (
             <div className="Home">
-                <h2>Home page</h2>
-                Current Block: {this.state.curr_block}
+                <h1>Transactions</h1>
+                Transactions: {txCount}
                 <table>
                     <thead>
                         <tr>
                             <th>Block no</th>
-                            <th>Hash</th>
+                            <th>Transactions</th>
+                            <th> Hash</th>
                         </tr>
                     </thead>
                     <tbody>
